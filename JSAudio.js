f81d7bb779770d9bf675ffa2be228ec29d017db3
dolymood <dolymood@gmail.com>
2013-07-17 21:46:39 +0800

    add

---
+++ JSAudio.js
@@ -0,0 +1,21 @@
+(function(win, doc) {
+
+	function JSAudio(options) {
+		this.audio = doc.createElement('audio');
+	}
+
+	JSAudio.prototype = {
+		
+		constructor: JSAudio,
+
+		play: function() {
+
+		},
+
+		pause: function() {
+
+		}
+
+	};
+
+})(window, document);
